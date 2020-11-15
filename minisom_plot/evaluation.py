9922a9652d674ccc0fbc65af39be3a5796f1d83e
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-11-15 19:29:04 -0300

    add umatrix, and neuron grid labeled

--- minisom_plot/evaluation.py
+++ minisom_plot/evaluation.py
@@ -3,6 +3,7 @@ import numpy as np
 import sklearn.datasets
 
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 
 def umatrix(som_model, use_colorbar=True, **kwargs):
@@ -21,7 +22,60 @@ def umatrix(som_model, use_colorbar=True, **kwargs):
     im = plt.imshow(som_model.distance_map(), **kwargs)
     
     if use_colorbar: plt.colorbar(im)
-              
+
+
+def umatrix_labeled(som_model, 
+                    data, 
+                    labels, 
+                    colors, 
+                    markers, 
+                    use_colorbar=True,
+                    plot_lbl_args=None,
+                    **kwargs):
+    """Plot a U-Matrix with labels in each pixel
+    
+    Args:
+        som_model (minisom.MiniSom): MiniSom Model
+        
+        data (np.ndarray): n-dimensional data to estimulate neurons and generate
+        activation map
+        
+        labels (np.ndarray): 1-dimensional data with label of data. Must be a
+        discrete values (e. g. 0, 1, 2, ...)
+        
+        colors (list): List of color to use in each class
+        
+        markers (list): List of markers to use in each class
+        
+        use_colorbar (bool): Flag to enable colorbar on figure plot
+        
+        plot_lbl_args (dict): Parameters to matplotlib.pyplot.plot function used
+        on plot labels
+        
+        kwargs (dict): Parameters to matplotlib.pyplot.imshow function
+    Returns:
+        None
+    """
+    
+    if not plot_lbl_args:
+        plot_lbl_args = {
+            'markerfacecolor': 'None',
+            'markersize': 12,
+            'markeredgewidth': 2
+        }
+    
+    im = plt.imshow(som_model.distance_map(), **kwargs)
+    if use_colorbar: plt.colorbar(im)
+    
+    for idx, de in enumerate(data):
+        label_idx = labels[idx] - 1
+        
+        winner = som_model.winner(de)
+        plt.plot(winner[1], winner[0], 
+                 markers[label_idx], 
+                 markeredgecolor=colors[label_idx],
+                 **plot_lbl_args)
+
 
 def hitmap(som_model, data, use_colorbar=True, **kwargs):
     """Plot Self-organizing map hitmap
@@ -76,3 +130,34 @@ def heatmap(som_model, feature_names, grid_spec, use_colorbar=True, **kwargs):
         im = plt.imshow(weights[:, :, i], **kwargs)
         
         if use_colorbar: plt.colorbar(im)
+
+
+def grid_pie_labeled(som_model, data, labels_name) -> tuple:
+    """
+    
+    Args:
+        som_model (minisom.MiniSom): MiniSom Model
+        
+        data (np.ndarray): n-dimensional data to estimulate neurons and generate
+        activation map
+        
+        labels_name (np.ndarray): 1-dimensional data with name of data label. Must be a
+        string values (e. g. 'classe1', 'classe2', ...)
+    
+    Returns:
+        tuple: Tuple with patches and texts used in each plotted neuron
+    """
+    
+    patches, texts = None, None
+    labels_map = som_model.labels_map(data, labels_name)
+    n_neurons, m_neurons = som_model.get_weights().shape[0:2]
+
+
+    grid_spec = gridspec.GridSpec(n_neurons, m_neurons, plt.gcf())
+    for position in labels_map.keys():
+        label_fracs = [labels_map[position][l] for l in labels_name]
+        plt.subplot(grid_spec[n_neurons-1-position[1],
+                             position[0]], aspect=1)
+        patches, texts = plt.pie(label_fracs)
+
+    return (patches, texts)
